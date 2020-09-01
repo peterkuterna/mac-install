bf502f86d98098f0030eac528ca2c1c42cca7c4f
Peter Kuterna <peter@kuterna.be>
2020-09-01 12:13:05 +0200

    Share android SDK and NDK in studio itself

--- scripts/android.sh
+++ scripts/android.sh
@@ -1,9 +1,11 @@
 echo
 echo "Installing Android tools"
+#brew cask install android-sdk
+#brew cask install android-ndk
+brew cask install android-platform-tools
 brew cask install android-file-transfer
 brew cask install jd-gui
 brew install apktool
 brew install dex2jar
 brew install jadx
 brew install scrcpy
-brew cask install android-platform-tools
